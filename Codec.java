f279176984db3dd49979473034c6ac853b8c9d2a
Hillary T <>
2021-04-04 02:20:33 +0000

    Initial commit

---
+++ Codec.java
@@ -0,0 +1,19 @@
+public class Codec {
+HashMap<String, String> map = new HashMap<String, String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+            String charMap = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    String token = "";
+    int num = 0;
+    for (int i = 0; i < 6; i++) {
+      num = (int)Math.floor(Math.random() * 62);
+        token += charMap.charAt(num);
+    }
+    String tinyUrl = "https://tinyurl.com/" + token;
+     map.put(tinyUrl, longUrl);
+     return tinyUrl;
+  }
+       public String decode(String shortUrl) {
+        return map.get(shortUrl);
+    }
+    }
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+  public static void main(String[] args) {
+    Codec yay = new Codec();
+    yay.encode("https://www.youtube.com/");
+  }
+}
