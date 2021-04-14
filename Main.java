9a88a85e6c8b8c0f3d0d23c5f38a917472f17d77
Hillary <htong0001@gmail.com>
2021-04-13 19:19:51 -0700

    majority nums added

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 import duplicate.*;
 import onefold.*;
+import majority.*;
 
 class Main {
   public static void main(String[] args) {
     Codec yay = new Codec();
     yay.decode(yay.encode("https://www.youtube.com/watch?v=pBdKnPcNyl8"));
-    int[] nums = {1,2,2,5,6,6,5,7,7};
+    int[] nums = {1,2,2,5,6,6,5,7,7,7,7};
     Duplicate sad = new Duplicate();
    System.out.println(sad.containsDuplicate(nums));
    singleNum welp = new singleNum();
    System.out.println("The only unique number in this array is " + welp.findSingle(nums));
+   majorityNums fan = new majorityNums();
+   System.out.println("The mode is:" + fan.findMajority(nums));
   }
 }
--- majority/majorityNums.java
+++ majority/majorityNums.java
@@ -4,15 +4,21 @@ import java.util.*;
 public class majorityNums {
     public int findMajority(int []nums){
         int most = 0;
-        //int count = 0;
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-        int current = nums[i];
-        if(current == nums[i+1]){
-           //count++;
-        }
-        }
-        return most;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(nums[i])) {
+        map.put(nums[i], map.get(nums[i]) + 1);
+      } else {
+        map.put(nums[i], 1);
+      }
+    }
+    for (Map.Entry<Integer, Integer> set : map.entrySet()) {
+      if (set.getValue() > nums.length/2) {
+        most = set.getKey();
+      }
+      continue;
+    }
+    return most;
     }
 }
 
