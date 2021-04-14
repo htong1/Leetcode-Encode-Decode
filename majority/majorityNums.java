5fc180dc17a9f28466ecc9757c2e83afb97f1bfe
Hillary <htong0001@gmail.com>
2021-04-13 19:21:06 -0700

    majority nums finished

--- majority/majorityNums.java
+++ majority/majorityNums.java
@@ -21,15 +21,3 @@ public class majorityNums {
     return most;
     }
 }
-
-//count current number frequency. 
-/* int most = 0;
-        int count = 0;
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-        int current = nums[i];
-        if(current == nums[i+1]){
-           count++;
-        }
-        }
-        return most;*/
